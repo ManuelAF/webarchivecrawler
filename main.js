d6e44c86f3ac23335429850bce223f81f85717e4
Manuel Alvarez Fernandez <manolo@el-mundo.net>
2014-02-20 12:39:59 +0100

    http.get

--- main.js
+++ main.js
@@ -1,44 +1,54 @@
+var http = require('http');
 var async = require('async');
-var request = require('request');
 var fs = require('fs');
 var colors = require('colors');
 
 var limit = 30;
 var URL = 'http://web.archive.org/web/timemap/link/http://www.elmundo.es';
 
+function get(URL, cb) {
+    http.get(URL,function (res) {
+
+        var body;
+
+        res.setEncoding('ascii');
+
+        res.on('data',function (chunk) {
+            body += chunk;
+        }).on('end', function () {
+                cb(null, body);
+            })
+
+    }).on('error', function (err) {
+            cb(err);
+        });
+}
+
 async.waterfall([
     function (cb) {
-        request(URL, function (err, response, body) {
-            if (!err && response.statusCode == 200) {
-                cb(null, body);
-            } else {
-                cb(err);
-            }
-        });
+        get(URL, cb);
     }, function (body, cb) {
+
         var r = /^<(http:\/\/web.archive.org\/web\/\d+\/.*?)>;/gm;
         var n = /^http:\/\/web.archive.org\/web\/(\d+)\/.*?/;
         var url, urls = [];
-        var filename;
-        while ((url = r.exec(body)) !== null) { urls.push(url[1]); }
+        while ((url = r.exec(body)) !== null) {
+            urls.push(url[1]);
+        }
         async.mapLimit(urls, limit, function (url, cb) {
-            request(url, function (err, response, body) {
-                if (!err && response.statusCode == 200) {
-                    filename = n.exec(url);
-                    fs.writeFile('tmp/' + filename[1] + '.html', body, { encoding : 'ascii' }, function (err) {
-                        console.log(url.green);
-                        cb(err, 'nada');
-                    });
-                } else {
-                    console.log(url.red);
-                    cb(err);
-                }
+            get(url, function () {
+                var filename = n.exec(url);
+                fs.writeFile('tmp/' + filename[1] + '.html', body, { encoding : 'ascii' }, function (err) {
+                    console.log(url.green);
+                    cb(err, 'nada');
+                });
             });
         }, function (err, results) {
             cb(err, results);
         });
-    }], function(err, results) {
-        if (err) {
-            console.log(err);
-        }
-    });
+
+    }], function (err, results) {
+    if (err) {
+        console.log(err);
+    }
+});
